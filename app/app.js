74a06e314013112b78f1746625578c3ffad30a84
marco-ponds <marcoponds@gmail.com>
2016-09-06 00:18:51 +0100

    working on bars

--- app/app.js
+++ app/app.js
@@ -1,8 +1,18 @@
+function randomInt(min,max)
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
 function Bar(element) {
     this.element = element;
     this.canvas = document.getElementById(element);
     this.ctx = this.canvas.getContext('2d');
     this.levels = [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5];
+    this.angles = [];
+    // creating random angles
+    for (var i=0; i<this.levels.length; i++) {
+        this.angles.push(randomInt(0, 2) - 1);
+    }
     this.level = 0.5;
     this.height = this.canvas.height;
     this.width = this.canvas.width;
@@ -11,23 +21,27 @@ function Bar(element) {
 
 Bar.prototype = {
     render: function() {
+        // clear canvas
+        this.ctx.clearRect(0, 0, this.width, this.height);
+
         var barWidth = 5;
         var barHeight;
         var x = 15;
 
         for(var i = 0; i < this.levels.length; i++) {
-            barHeight = (this.levels[i]*this.height) + (Math.sin(this._angle) * 20);
+            barHeight = (this.levels[i]*this.height) + (Math.sin(this.angles[i]) * 5);
 
             this.ctx.fillStyle = 'rgb(255, 255, 255)';
             this.ctx.fillRect(x, this.height-barHeight, barWidth, barHeight);
 
             x += barWidth + 8;
+            this.angles[i] += 0.1;
         }
         // shifting and pushing level
         this.levels.shift();
         this.levels.push(app[this.element].level);
         // increasing angle
-        this.angle ++;
+        this._angle += 0.1;
     },
 
     inc: function(amount) {
